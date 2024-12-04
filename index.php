2ba5b8b22b19f91f00e757c832d0b056260fa7df
Ange Arsene <nkenmandenga@gmail.com>
2024-12-04 17:36:16 +0000

    add comments

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ require './vendor/autoload.php';
 
 use MtnMomoPaymentGateway\Core\Application;
 
+/**
+ * Entry point for the MtnMomo Payment Gateway application
+ */
+
+// Instantiate the main Application class to initialize the MtnMomo Payment Gateway
 $application = new Application();
--- src/Core/Application.php
+++ src/Core/Application.php
@@ -3,19 +3,32 @@
 namespace MtnMomoPaymentGateway\Core;
 
 use Dotenv\Dotenv;
+use Ramsey\Uuid\Uuid;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
-use Ramsey\Uuid\Uuid;
 
+/**
+ * Class Application
+ * 
+ * Represents the main application class for handling MtnMomo payments.
+ */
 class Application 
 {
+    /** @var string The home directory path */
     public static string $HOME_DIR;
 
+    /** @var Client HTTP client */
     private Client $http_client;
     
+    /** @var string The primary key */
     protected static string $PRIMARY_KEY;
+
+    /** @var string The secondary key */
     protected static string $SECONDARY_KEY;
 
+    /**
+     * Constructor method
+     */
     public function __construct() 
     {
         self::$HOME_DIR = dirname(dirname(__DIR__));
@@ -27,14 +40,20 @@ class Application
         var_dump($this->create_api_key());
     }
 
+    /**
+     * Initializes the application by setting primary and secondary keys
+     */
     private function bootstrap(): void
     {
         self::$PRIMARY_KEY   = $this->env()->primary_key;
         self::$SECONDARY_KEY = $this->env()->secondary_key;
-        
-        
     }
 
+    /**
+     * Loads environment variables
+     *
+     * @return object An object containing environment variables
+     */
     private function env(): object
     {
         $dotenv = Dotenv::createImmutable(self::$HOME_DIR);
@@ -43,6 +62,11 @@ class Application
         return (object) $_ENV;
     }
 
+    /**
+     * Generates a user reference ID
+     *
+     * @return string The generated user reference ID
+     */
     protected function user_reference_id(): string
     {
         $uuid = Uuid::uuid4();
@@ -50,6 +74,11 @@ class Application
         return $this->write_to_env('user_reference_id', $uuid);
     }
 
+    /**
+     * Creates an API key
+     *
+     * @return string The generated API key
+     */
     protected function create_api_key(): string
     {
         if ($this->create_api_user()) {
@@ -70,6 +99,11 @@ class Application
         }
     }
 
+    /**
+     * Creates an API user
+     *
+     * @return bool Returns true if API user creation was successful; otherwise, false
+     */
     private function create_api_user(): bool
     {
         $callback_url = $this->env()->callback_url;
@@ -94,21 +128,28 @@ class Application
         } catch (\Throwable $e) { return false; }
     }
     
+    /**
+     * Writes a key-value pair to the .env file
+     *
+     * @param string $key The key to write
+     * @param string $value The value to write
+     * @return string The value that was written
+     */
     private function write_to_env(string $key, string $value): string
     {
-        $envFile = self::$HOME_DIR . DIRECTORY_SEPARATOR .'.env';
+        $envFile = self::$HOME_DIR . DIRECTORY_SEPARATOR . '.env';
 
         // Read the existing content of the .env file
         $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         
-        // Initialize a variable to store the found value
+        // Initialize variables for found value and flag
         $foundValue = null;
         $found = false;
 
         // Search for the key in the existing lines
         foreach ($lines as &$line) {
             if (strpos($line, $key . '=') === 0) {
-                $foundValue = trim(explode('=', $line, 2)[1]); // Get the value
+                $foundValue = trim(explode('=', $line, 2)[1]); // Get the current value
                 $found = true;
                 break;
             }
@@ -120,7 +161,7 @@ class Application
                 return $foundValue;
             } else {
                 $lineIndex = array_search($line, $lines);
-                $lines[$lineIndex] = "$key=$value"; // Update the line
+                $lines[$lineIndex] = "$key=$value"; // Update the line with new value
                 file_put_contents($envFile, implode(PHP_EOL, $lines) . PHP_EOL);
                 return $value; // Return the newly set value
             }
